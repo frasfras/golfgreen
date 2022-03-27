042352f1496898772a8e069c9b04cfa178a0716e
frasfras <65541080+frasfras@users.noreply.github.com>
2022-03-27 20:26:25 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+   <h1>Formulate Waterplan </h1>
+    <pega-embed id='theEmbed' action='createCase' caseTypeID='MyOrg-GolfGreen-Work-Waterplan' casePage='full' appAlias='golf-green' pegaServerUrl='https://sw5fyxc4.pegace.net/prweb/' staticContentUrl='https://community.constellation.pega.io/c11n/' authService='SSO' clientId='11445058306307655452' style='width:100%'>
+</pega-embed>
 </template>
